933cd023f31a2d62e100d7d1a0f360ad9f1db857
Ye Li <ye.li@yunzhanghu.com>
2017-11-21 14:30:22 +0800

    t

--- disk_exporter.go
+++ disk_exporter.go
@@ -3,15 +3,34 @@ package main
 import (
 	"fmt"
 	"flag"
-	"github.com/liyehaha/hostmonitor/config"
+
+	//"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/node_exporter/collector"
+
+	config "github.com/liyehaha/hostmonitor/config"
 )
 
 const (
 	namespace	          = "monitor"
 )
 
+func diskmonitor() {
+
+	disk, _ := collector.NewDiskstatsCollector()
+
+	fmt.Println(disk)
+	fmt.Println(1)
+	//return err
+}
+
 func main() {
-	promserver = config.PromPushGW.Server
+
+	var config_path = flag.String("conf", "config.yaml", "config file")
+	config.Load(*config_path)
+	fmt.Println(2)
+	promserver := config.Monitor.PromPushGW
 	fmt.Println(promserver)
+	
+	diskmonitor()
 }
 
